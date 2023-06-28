f7c5010a7079c0d8036cf1424c448016d43ca921
Junfeng Ou <jun@Junfengs-MacBook-Pro.local>
2023-06-28 12:44:40 -0500

    Updated all the configurations and latest expo go dependencies

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -6,7 +6,8 @@ import {
   Avatar,
   Heading,
   IconButton,
-  useColorModeValue
+  useColorModeValue,
+  ScrollView
 } from 'native-base'
 import { DrawerContentComponentProps } from '@react-navigation/drawer'
 import AnimatedColorBox from './animated-color-box'
@@ -39,47 +40,49 @@ const Sidebar = (props: DrawerContentComponentProps) => {
       bg={useColorModeValue('blue.50', 'darkBlue.800')}
       p={7}
     >
-      <VStack flex={1} space={2}>
-        <HStack justifyContent="flex-end">
-          <IconButton
-            onPress={handlePressBackButton}
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <VStack flex={1} space={2}>
+          <HStack justifyContent="flex-end">
+            <IconButton
+              onPress={handlePressBackButton}
+              borderRadius={100}
+              variant="outline"
+              borderColor={useColorModeValue('blue.300', 'darkBlue.700')}
+              _icon={{
+                as: Feather,
+                name: 'chevron-left',
+                size: 6,
+                color: useColorModeValue('blue.800', 'darkBlue.700')
+              }}
+            />
+          </HStack>
+          <Avatar
+            source={require('../assets/profile-image.png')}
+            size="xl"
             borderRadius={100}
-            variant="outline"
-            borderColor={useColorModeValue('blue.300', 'darkBlue.700')}
-            _icon={{
-              as: Feather,
-              name: 'chevron-left',
-              size: 6,
-              color: useColorModeValue('blue.800', 'darkBlue.700')
-            }}
+            mb={6}
+            borderColor="secondary.500"
+            borderWidth={3}
           />
-        </HStack>
-        <Avatar
-          source={require('../assets/profile-image.png')}
-          size="xl"
-          borderRadius={100}
-          mb={6}
-          borderColor="secondary.500"
-          borderWidth={3}
-        />
-        <Heading mb={4} size="xl">
-          Takuya Matsuyama
-        </Heading>
-        <MenuButton
-          active={currentRoute === 'Main'}
-          onPress={handlePressMenuMain}
-          icon="inbox"
-        >
-          Tasks
-        </MenuButton>
-        <MenuButton
-          active={currentRoute === 'About'}
-          onPress={handlePressMenuAbout}
-          icon="info"
-        >
-          About
-        </MenuButton>
-      </VStack>
+          <Heading mb={4} size="xl">
+            Jun Ou
+          </Heading>
+          <MenuButton
+            active={currentRoute === 'Main'}
+            onPress={handlePressMenuMain}
+            icon="inbox"
+          >
+            Tasks
+          </MenuButton>
+          <MenuButton
+            active={currentRoute === 'About'}
+            onPress={handlePressMenuAbout}
+            icon="info"
+          >
+            About
+          </MenuButton>
+        </VStack>
+      </ScrollView>
       <Center>
         <ThemeToggle />
       </Center>
